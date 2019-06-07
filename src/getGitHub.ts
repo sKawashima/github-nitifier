1d8d4ec9106d84651e0358470f360ab2145adab7
sKawashima <bysk.333@gmail.com>
2019-06-07 11:38:54 +0900

    complete make function

--- src/getGitHub.ts
+++ src/getGitHub.ts
@@ -1,3 +1,5 @@
+'use strict'
+
 import * as argv from 'argv'
 const target: string = argv.run()['targets'][0]
 
@@ -9,10 +11,23 @@ const octokit = new Octokit({
   auth: '982f439cbd9ebd51073eee49e31c41f0a4a72fa9'
 })
 
-octokit.issues.get({
-  owner: 'mwed',
-  repo:  'mwed-design',
-  issue_number: 70
-}).then((res) => {
-  console.log(res)
-})
+octokit.issues
+  .get({
+    owner: 'mwed',
+    repo: 'mwed-design',
+    issue_number: 70
+  })
+  .then(res => {
+    console.log(res)
+  })
+
+// import * as notifier from 'node-notifier'
+// notifier.notify({
+//     title: url,
+//     message: 'メッセージ',
+// })
+// import * as opener from "opener";
+// notifier.on('click', function (notifierObject, options) {
+//     console.log('click')
+//     opener(url)
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,60 @@
 import * as argv from 'argv'
-import * as opener from 'opener'
 
-const target: string = argv.run()['targets'][0]
+const url = new URL(argv.run()['targets'][0])
+console.log(`Start monitoring: ${url}`)
 
-console.log(target)
+if (url.host !== 'github.com') {
+  console.error('please check url')
+  process.exit()
+}
+
+const owner = url.pathname.split('/')[1]
+const repo = url.pathname.split('/')[2]
+const type = url.pathname.split('/')[3]
+const number = Number(url.pathname.split('/')[4])
+
+import * as Octokit from '@octokit/rest'
+
+const octokit = new Octokit({
+  auth: '982f439cbd9ebd51073eee49e31c41f0a4a72fa9'
+})
+
+let comments:number = 0
 
 import * as notifier from 'node-notifier'
+import 'opener'
 
-notifier.notify({
-    title: target,
-    message: 'メッセージ',
-})
+const notice = (comments_n) => {
+  notifier.notify({
+      title: url.toString(),
+      message: `更新されました: ${comments_n}`,
+  })
 
-notifier.on('click', function (notifierObject, options) {
-    console.log('click')
-    opener('https://github.com/mwed/mwed-design/issues/70')
-})
+  notifier.on('click', function (notifierObject, options) {
+      console.log('click')
+      opener(url.toString())
+  })
+}
+
+const checkIssue = () => {
+  octokit.issues
+    .get({
+        owner: owner,
+        repo: repo,
+        issue_number: number
+    })
+    .then(res => {
+      const res_comments = res['data']['comments']
+      if (res_comments !== comments) {
+        notice(res_comments)
+        comments = res_comments
+        console.log(`comments: ${res_comments}`)
+      }
+    })
+}
+
+checkIssue()
+
+import * as cron from 'node-cron'
+
+cron.schedule('*/10 * * * * *', () => checkIssue())
